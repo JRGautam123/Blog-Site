66845d9e7838b6f486ea95cf166707ec511472ee
JRGautam123 <jaya.ram@pu.edu.np>
2025-04-20 12:51:56 +0545

    add latest blogs features to home page

--- postapp/views.py
+++ postapp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def render_landing_page(request):
-    return render(request=request,template_name='base.html')
+def render_home_page(request):
+    return render(request=request,template_name='postapp/home_page.html')
