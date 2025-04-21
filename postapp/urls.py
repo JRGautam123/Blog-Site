57283710fbd012e3a870b781cce5a96e5b4b2e7f
JRGautam123 <jaya.ram@pu.edu.np>
2025-04-21 16:53:35 +0545

    Add see articles features

--- postapp/urls.py
+++ postapp/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path("",views.render_home_page,name='home_page'),
+    path("articles",views.render_article_page,name='article_page')
 ]
--- postapp/views.py
+++ postapp/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def render_home_page(request):
     return render(request=request,template_name='postapp/home_page.html')
+
+
+def render_article_page(request):
+    return render(request=request,template_name='postapp/articles.html')
