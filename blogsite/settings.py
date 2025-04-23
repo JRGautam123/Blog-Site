ccb43b5a346ce70d9fed57ff0389942a7e5ac2bf
JRGautam123 <jaya.ram@pu.edu.np>
2025-04-23 14:50:43 +0545

    add user registration  and login feature

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -131,3 +131,7 @@ MEDIA_ROOT=BASE_DIR/'uploads'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL='home_page'
+LOGOUT_REDIRECT_URL='login'
+LOGIN_URL='login'
--- userapp/urls.py
+++ userapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .import views
+from django.contrib.auth.views import LoginView,LogoutView
 
 urlpatterns = [
+    path("login/",LoginView.as_view(template_name='userapp/login_page.html'),name='login'),
+    path("logout/",LogoutView.as_view(next_page='login'),name='logout'),
     path('register',views.register,name='user-register')
 ]
