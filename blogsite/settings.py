f1354ef314cd0f146719a78f89173604a653a92b
Jayaram <jrgautam20@gmail.com>
2025-10-15 16:33:15 +0545

    Fix: add .env file and removed old migrations files

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -11,25 +11,25 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import environ
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-twbd+!t*)99wq#jm^5p8co3aiy#cqtj*kdqj30g3iqbqi1l4p+'
+env = environ.Env(DEBUG=(bool , False))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'localhost',
-    '192.168.1.69', 
-]
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': env.str('DB_NAME'),
+        "USER": env.str("DB_USER"),
+        "PASSWORD": env.str("DB_PASS"),
+        "HOST": env.str('DB_HOST'),
+        "PORT": env.str("DB_PORT")
     }
 }
 
--- userapp/signals.py
+++ userapp/signals.py
@@ -7,7 +7,6 @@ from .models import UserProfile
 @receiver(post_save,sender=User)
 def create_user_profile(sender,instance,created,**kwargs):
     if created:
-        print('signlas is arrived')
         UserProfile.objects.create(user=instance)
 
 # @receiver(post_save,sender=User)
